2d02275863b9b86f6526e1a9c5ac4c105c7ab2f2
AlexandruPaiul <alexandru.paiul@gmx.de>
2021-03-30 10:16:01 +0200

    done

---
+++ start.js
@@ -0,0 +1,14 @@
+// Task one
+
+console.log("Hello World");
+// console.log("This is my first js file");
+
+// Task two
+
+
+// node start.js
+// /home/dci/Documents/JavaScriptStuff/pb-language-statements-AlexandruPaiul/start.js:2
+// console.log"Hello World";
+//            ^^^^^^^^^^^^^
+
+// SyntaxError: Unexpected string
